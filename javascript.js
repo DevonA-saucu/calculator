12ec0c3c6c6d51bc4cc259aec2f6f880a04bc3a4
Devon <60799686+DevonA-saucu@users.noreply.github.com>
2024-07-16 14:09:01 -0500

    Add functionality to visual calculator
    Still working some bugs with return values out
    and have yet to implement keyboard functionality

--- javascript.js
+++ javascript.js
@@ -1,11 +1,48 @@
 let firstNum = 0;
 let secondNum = 0;
 let operator = "";
+let equated = 0;
 
 let buttons = document.querySelectorAll(".button");
+let mainDisplay = document.querySelector(".main");
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", () => {
+        if (buttons[i].classList.contains("number")) {
+            if (mainDisplay.textContent === firstNum || mainDisplay.textContent === equated) {
+                mainDisplay.textContent = "";
+            }
+
+            if (mainDisplay.textContent.length === 13) {
+                mainDisplay.textContent = mainDisplay.textContent;
+            } else {
+                mainDisplay.textContent += buttons[i].textContent;
+            }
+
+        } else if (buttons[i].textContent === "AC") {
+            mainDisplay.textContent = "";
+            firstNum = 0;
+            secondNum = 0;
+            operator = "";
+            equated = 0;
+
+        } else if (buttons[i].textContent === "del") {
+            mainDisplay.textContent = mainDisplay.textContent.slice(0, mainDisplay.textContent.length - 1);
+
+        } else if (buttons[i].classList.contains("operate")) {
+            firstNum = mainDisplay.textContent;
+            operator = buttons[i].textContent;
+
+        } else if (buttons[i].textContent === "=") {
+            secondNum = mainDisplay.textContent;
+            mainDisplay.textContent = operate(operator, firstNum, secondNum);
+            equated = mainDisplay.textContent;
+        }
+    });
+}
 
 function add(numOne, numTwo) {
-    return numOne + numTwo;
+    return +numOne + +numTwo;
 }
   
 function subtract(numOne, numTwo) {
@@ -23,13 +60,13 @@ function divide(numOne, numTwo) {
 function operate(operand, numOne, numTwo) {
     switch(operand) {
         case "+":
-            add(numOne, numTwo);
+            return add(numOne, numTwo);
         case "-":
-            subtract(numOne, numTwo);
+            return subtract(numOne, numTwo);
         case "*":
-            multiply(numOne, numTwo);
+            return multiply(numOne, numTwo);
         case "/":
-            divide(numOne, numTwo);
+            return divide(numOne, numTwo);
     }
 }
   
