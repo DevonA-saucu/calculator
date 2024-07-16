a4096fad73c6bbd8f0687c81570d414e72108d91
Devon <60799686+DevonA-saucu@users.noreply.github.com>
2024-07-16 02:03:41 -0500

    Finalize calculator look and create basic functions

---
+++ javascript.js
@@ -0,0 +1,24 @@
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
+
+function add(numOne, numTwo) {
+    return numOne + numTwo;
+}
+  
+function subtract(numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+function multiply(numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+function divide(numOne, numTwo) {
+    return numOne / numTwo;
+}
+
+function operate(operand, numOne, numTwo) {
+    
+}
+  
